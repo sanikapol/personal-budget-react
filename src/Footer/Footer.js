590dec8b3035864379b25feca27b7361d78a6882
sanikapol <50167925+sanikapol@users.noreply.github.com>
2020-10-12 14:35:30 -0400

    Added React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+      <div className="center">
+          All rights reserved &copy; Sanika Pol
+      </div>
+    </footer>
+    
+  );
+}
+
+export default Footer;
